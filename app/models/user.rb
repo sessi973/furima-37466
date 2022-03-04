61f04ca83d440e399eea96f08a49c074a1dcdb7b
sessi973 <hatano.seiko@gmail.com>
2022-03-04 13:49:59 +0900

    モデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+
+  validates :birth_date, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+
 end
+
