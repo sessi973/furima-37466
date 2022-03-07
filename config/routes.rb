3baed4c7e80004f7387a781af4b78e02e5f71e21
sessi973 <hatano.seiko@gmail.com>
2022-03-07 15:44:54 +0900

    ユーザー新規登録　データベースへの保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :users, only: [:edit, :update]
+  resources :registrations, only: [:index,:new, :create, :edit, :update]
 end
