f8917f10aec0733b8f4543dfd4eb61f88b50a6a8
sessi973 <hatano.seiko@gmail.com>
2022-03-07 18:21:15 +0900

    usersモデルのバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true, length: { minimum: 6 }
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true do
+  validates :nickname
+  validates :email
+  validates :password,length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }
+  validates :password_confirmation, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 
   validates :birth_date, presence: true
-
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -20,4 +21,3 @@ class User < ApplicationRecord
 
 
 end
-
