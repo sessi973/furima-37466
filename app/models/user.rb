013827fd501c001ebc35d8d88292d203fce5ea8b
sessi973 <hatano.seiko@gmail.com>
2022-03-07 21:23:08 +0900

    Rspec Factorybot,Fackerの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-
   validates :birth_date, presence: true
   end
   # Include default devise modules. Others available are:
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+ RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+   describe "ユーザー新規登録" do
+     it "nicknameが空だと登録できない" do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+     it "emailが空では登録できない" do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+     end
+     it 'passwordが空だと登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+   end
+ end
