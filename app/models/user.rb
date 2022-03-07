a4614a28d05d1adecfe3277f9e26c5eac7a024f7
sessi973 <hatano.seiko@gmail.com>
2022-03-07 19:55:29 +0900

    Rubocopによりスペースを作りました

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   with_options presence: true do
   validates :nickname
   validates :email
-  validates :password,length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }
+  validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }
   validates :password_confirmation, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i }
   validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
