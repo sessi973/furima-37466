68712b2c5d0b3438f75017e3c18ea03525bddede
sessi973 <hatano.seiko@gmail.com>
2022-03-10 15:29:08 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions  
 has_one    :order
-belongs_to :user
+belongs_to :user 
+belongs_to :category 
+belongs_to :sales_status
+belongs_to :shipping_fee_status
+belongs_to :prefecture
+belongs_to :scheduled_delivery 
 has_one_attached :image
 
 validates :image, presence: true
 validates :name, presence: true
 validates :info, presence: true                   
-validates :category_id, presence: true            
-validates :sales_status_id, presence: true        
-validates :shipping_fee_status_id, presence: true  
-validates :prefecture_id, presence: true         
-validates :scheduled_delivery_id, presence: true  
+validates :category_id, presence: true, numericality: { other_than: 1 }          
+validates :sales_status_id, presence: true, numericality: { other_than: 1 }         
+validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 }   
+validates :prefecture_id, presence: true, numericality: { other_than: 1 }          
+validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 }  
 validates :price, presence: true                   
 validates :user, presence: true
+
 
 end
