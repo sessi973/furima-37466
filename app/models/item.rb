94a00104af8ed013361f3b6e90b141dcba3595d2
sessi973 <hatano.seiko@gmail.com>
2022-03-10 15:32:58 +0900

    バリデーション　メッセージ追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ has_one_attached :image
 validates :image, presence: true
 validates :name, presence: true
 validates :info, presence: true                   
-validates :category_id, presence: true, numericality: { other_than: 1 }          
-validates :sales_status_id, presence: true, numericality: { other_than: 1 }         
-validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 }   
-validates :prefecture_id, presence: true, numericality: { other_than: 1 }          
-validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 }  
+validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}          
+validates :sales_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}         
+validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}   
+validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}          
+validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}  
 validates :price, presence: true                   
 validates :user, presence: true
 
