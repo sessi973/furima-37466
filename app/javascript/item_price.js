ec4207973b98a058ef951e63786ec5beb89a0b08
sessi973 <hatano.seiko@gmail.com>
2022-03-12 13:29:59 +0900

    計算式（コードレビューの修正）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,18 @@
 function tax (){
   const itemPrice  = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const fee = itemPrice.value * 0.1;
-    const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${fee}`;
-    const SalesProfit = itemPrice.value - fee ;
-    const profit = document.getElementById("profit");
-    profit.innerHTML = `${SalesProfit}`;
-  });
+  itemPrice.addEventListener("input", () => {
+    const inputValue = itemPrice.value;
+    console.log(inputValue);
+
+    const addTaxValue= document.getElementById("add-tax-price");
+    const addTax = Math.floor(inputValue * 0.1)
+    addTaxValue.innerHTML = addTax;
+     console.log(addTaxValue);
+
+    const profit = document.getElementById("profit")
+    profit.innerHTML = (Math.floor(inputValue - addTax));
+     console.log(profit);
+  })
 }
 
 window.addEventListener('load', tax);
