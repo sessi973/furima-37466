8319dc380f7907f602123d05fd9b2e287de68667
sessi973 <hatano.seiko@gmail.com>
2022-03-12 13:35:34 +0900

    バリデーションのリファクタリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions  
-#has_one    :order
-belongs_to :user 
-belongs_to :category 
-belongs_to :sales_status
-belongs_to :shipping_fee_status
-belongs_to :prefecture
-belongs_to :scheduled_delivery 
-has_one_attached :image
+ #has_one    :order
+ belongs_to :user 
+ belongs_to :category 
+ belongs_to :sales_status
+ belongs_to :shipping_fee_status
+ belongs_to :prefecture
+ belongs_to :scheduled_delivery 
+ has_one_attached :image
 
-validates :image, presence: true
-validates :name, presence: true
-validates :info, presence: true                   
-validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}          
-validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}         
-validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}   
-validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}          
-validates :scheduled_delivery_id, numericality: { other_than: 1 ,message: "can't be blank"}  
-validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }                  
+ with_options presence: true do
+ validates :image
+ validates :name
+ validates :info
+ validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }                  
+ end
 
-
-
+ with_options numericality: { other_than: 1 , message: "can't be blank"} do
+ validates :category_id        
+ validates :sales_status_id       
+ validates :shipping_fee_status_id
+ validates :prefecture_id        
+ validates :scheduled_delivery_id
+ end
+ 
 end
