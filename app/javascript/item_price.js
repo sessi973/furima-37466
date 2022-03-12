ce6f68f26a45f30f8befe0e6f09b64bb37eb4c3d
sessi973 <hatano.seiko@gmail.com>
2022-03-12 14:09:48 +0900

    デバッグ用の記述を削除

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,16 +2,13 @@ function tax (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("input", () => {
     const inputValue = itemPrice.value;
-    console.log(inputValue);
 
     const addTaxValue= document.getElementById("add-tax-price");
     const addTax = Math.floor(inputValue * 0.1)
     addTaxValue.innerHTML = addTax;
-     console.log(addTaxValue);
 
     const profit = document.getElementById("profit")
     profit.innerHTML = (Math.floor(inputValue - addTax));
-     console.log(profit);
   })
 }
 
