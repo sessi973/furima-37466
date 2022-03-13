13c9f0b297cf618e6c80b97f06069a465cd031ea
sessi973 <hatano.seiko@gmail.com>
2022-03-13 22:28:46 +0900

    編集ボタン、editアクション、ビューの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,14 @@ class ItemsController < ApplicationController
    def show
     @item = Item.find(params[:id])
    end
+
+   def edit
+    @item = Item.find(params[:id])
+   end
+
+   def update
+    
+   end
   private
 
   def item_params
