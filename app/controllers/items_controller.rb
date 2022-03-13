c4fdccbc916f4f625228c79c31607ee6f320a120
sessi973 <hatano.seiko@gmail.com>
2022-03-13 23:28:37 +0900

    遷移ページの制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show, :update ]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -29,9 +31,8 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to item_path(@item)
+    if@item.update(item_params)
+     redirect_to item_path(@item)
      else
       render :edit
     end
@@ -41,5 +42,15 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  @item = Item.find(params[:id])
+  unless  user_signed_in? && @item.user_id == current_user.id
+    redirect_to action: :index
+  end
+end
+def set_item
+  @item = Item.find(params[:id])
 end
 end
