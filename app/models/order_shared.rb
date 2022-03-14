71c32780aee9781bc19e2f55a56ad549b0c79fa8
sessi973 <hatano.seiko@gmail.com>
2022-03-14 22:37:47 +0900

    formオブジェクトの作成

--- app/models/order_shared.rb
+++ app/models/order_shared.rb
@@ -1,10 +1,21 @@
 class OrderShared
   include ActiveModel::Model
-  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order
+  attr_accessor :user_id, :item, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order
 
-  # ここにバリデーションの処理を書く
-
+  with_options presence: true do
+   validates :city
+   validates :addresses
+   validates :phone_number, format: {^0\d{10,11}$}
+    #validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+   validates :user_id
+   validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :building
   def save
     # 各テーブルにデータを保存する処理を書く
+ order = Order.create(item: item, user_id: user_id)
+ # 住所を保存する
+ Address.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order_id)
   end
 end
