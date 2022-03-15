cf78ab120d3990b534b84fe7b938b6a7f1fa551e
sessi973 <hatano.seiko@gmail.com>
2022-03-15 21:05:02 +0900

    sharedテーブルへの保存

--- app/models/order_shared.rb
+++ app/models/order_shared.rb
@@ -7,15 +7,14 @@ class OrderShared
    validates :addresses
    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
    validates :item_id
-   validates :user_id
-   
+   validates :user_id 
    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   def save
     # 各テーブルにデータを保存する処理を書く
- order = Order.create(item_id: item_id, user_id: user_id)
- # 住所を保存する
- Shared.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order_id)
+   order = Order.create(item_id: item_id, user_id: user_id)
+    # 住所を保存する
+   Shared.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
