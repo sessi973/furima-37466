af3eb5ee4f35449869d2cc134bef438fddb3c70c
sessi973 <hatano.seiko@gmail.com>
2022-03-15 13:50:51 +0900

    購入ページへのパス指定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,22 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
  
   def index
+    @order_shared = OrderShared.index
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(shared_params)
-    redirect_to root_path
-
+    @order_shared = OrderShared.create(order_params)
+    if @order_shared.valid?
+      @order_shared.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
-  end
-
-  def shared_params
-    params.permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(order_id: @order.id)
+    params.require(:order_shared).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id)
   end
 end
