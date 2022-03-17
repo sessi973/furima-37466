73d8ae29c19e606cd4dcfe00fde3974ad1d601d3
sessi973 <hatano.seiko@gmail.com>
2022-03-17 14:16:18 +0900

    テストコード修正

--- spec/models/order_shared_spec.rb
+++ spec/models/order_shared_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe OrderShared, type: :model do
        item = FactoryBot.create(:item)
       
       @order_shared = FactoryBot.build(:order_shared, user_id: user.id, item_id: item.id)
-      # sleep 1.0
     end
 
     context '購入できるとき' do
@@ -54,7 +53,7 @@ RSpec.describe OrderShared, type: :model do
       it '電話番号が空では登録できない' do
         @order_shared.phone_number = ''
         @order_shared.valid?
-        expect(@order_shared.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+        expect(@order_shared.errors.full_messages).to include("Phone number can't be blank")
       end
       it '電話番号が10桁以下では登録できない' do
         @order_shared.phone_number = 000000000
@@ -76,6 +75,17 @@ RSpec.describe OrderShared, type: :model do
         @order_shared.valid?
         expect(@order_shared.errors.full_messages).to include("Phone number is invalid")
       end
+      it 'userが紐付いていなければ購入できない' do
+        @order_shared.user_id = nil
+        @order_shared.valid?
+        expect(@order_shared.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐付いていなければ購入できない' do
+        @order_shared.item_id = nil
+        @order_shared.valid?
+        expect(@order_shared.errors.full_messages).to include("Item can't be blank")
+      end
+    
     end
   end
 end  
