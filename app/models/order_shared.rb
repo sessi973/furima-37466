4bd613c0e318c3b2f2db1d34612e98d85e5e1601
sessi973 <hatano.seiko@gmail.com>
2022-03-17 19:05:57 +0900

    詳細パスと　都道府県の訂正

--- app/models/order_shared.rb
+++ app/models/order_shared.rb
@@ -11,7 +11,7 @@ class OrderShared
    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
    validates :token
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
   def save
    order = Order.create(item_id: item_id, user_id: user_id)
    Shared.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
